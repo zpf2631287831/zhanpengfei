0c94752c77dfda1074727b824e485ce650d947a4
zpf <2631287831@qq.com>
2019-06-04 19:43:25 +0800

    创建dome1项目

--- demo1/booktest/models.py
+++ demo1/booktest/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+"""
+MVT 中的 M
+"""
 # Create your models here.
+class BookInfo(models.Model):
+    title=models.CharField(max_length=20)
+    pub_date=models.DateTimeField()
+
+class HeroInfo(models.Model):
+    name=models.CharField(max_length=20)
+    gender=models.BooleanField(default=True)
+    content=models.CharField(max_length=100)
+    #book作为外键关联到bookinfo表
+    book=models.ForeignKey(BookInfo,on_delete=models.CASCADE)
--- demo1/demo1/settings.py
+++ demo1/demo1/settings.py
@@ -29,7 +29,7 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
-
+#需要将创建的应用注册
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'booktest'
 ]
 
 MIDDLEWARE = [
